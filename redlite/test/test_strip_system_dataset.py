437ec6be04f0f4c2c2da9960cee20ed55f5cc7da
Mike Kroutikov <mkroutikov@innodata.com>
2024-02-12 08:12:19 -0500

    dataset wrapper to strip system messages

---
+++ redlite/test/test_strip_system_dataset.py
@@ -0,0 +1,47 @@
+from collections.abc import Iterator
+from redlite.dataset import StripSystemDataset
+from redlite import NamedDataset, DatasetItem
+
+
+class MockDataset(NamedDataset):
+    def __init__(self, *, name, labels={}, data):
+        self.labels = labels
+        self.data = data
+        self.name = name
+
+    def __len__(self) -> int:
+        return len(self.data)
+
+    def __iter__(self) -> Iterator[DatasetItem]:
+        yield from self.data
+
+
+def test_smoke():
+    dataset = MockDataset(
+        name="foo",
+        data=[
+            {
+                "id": "1",
+                "messages": [
+                    {"role": "system", "content": "I am helpful"},
+                    {"role": "user", "content": "Hello"},
+                ],
+                "expected": "boo",
+            },
+        ],
+    )
+
+    wrapped = StripSystemDataset(dataset)
+    assert wrapped.name == "foo-strip-system"
+    assert len(wrapped) == 1
+
+    data = list(wrapped)
+
+    assert len(data) == 1
+    assert data[0] == {
+        "id": "1",
+        "messages": [
+            {"role": "user", "content": "Hello"},
+        ],
+        "expected": "boo",
+    }
