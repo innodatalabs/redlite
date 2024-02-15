4cbdedf78cb99645ad2d33e08a5de74cb25d6198
Mike Kroutikov <mkroutikov@innodata.com>
2024-02-15 07:24:27 -0500

    suggesting changes

--- redlite/metric/_basic.py
+++ redlite/metric/_basic.py
@@ -1,3 +1,4 @@
+from typing import Literal
 from .. import NamedMetric
 from .util import normalize_string
 
@@ -10,19 +11,33 @@ class MatchMetric(NamedMetric):
     "Correct, because blah blah blah...". To give model full marks for longer and
     verbose answer, use this metric.
 
-    - **ignore_case** (`bool`) - when set to `True` will ignore text case. Deafult is `False`.
+    - **ignore_case** (`bool`, optional) - when set to `True` will ignore text case. Deafult is `False`.
 
-    - **ignore_punct** (`bool`) - when set to `True` punctuation symbols will be ignored.
+    - **ignore_punct** (`bool`, optional) - when set to `True` punctuation symbols will be ignored.
             Default is `False`.
 
-    - **normalize_white_space** (`bool`) - when set to `True`, normalizes white space by
+    - **normalize_white_space** (`bool`, optional) - when set to `True`, normalizes white space by
             replacing tabs and newlines with spaces and replacing multiple spaces to one. Also
             strips leading and trailing whitespace.
             Default is `False`.
+    - **match** (`Literal["exact", "prefix", "contains"]`, optional) - determines how strings are matched.
+
+        * `"exact"`: matches if expected and actual responses are the same
+        * `"prefix"`: matches if actual response starts with the expected string
+        * `"contains"`: matches if expected string is found anywhere in the actual response
+
+        Default is `"exact"`.
     """
 
-    def __init__(self, ignore_case=False, ignore_punct=False, normalize_whitespace=False):
-        name = "prefix"
+    def __init__(
+        self,
+        ignore_case=False,
+        ignore_punct=False,
+        normalize_whitespace=False,
+        match: Literal["exact", "contains", "prefix"] = "prefix"):
+        if match not in ("prefix", "contains", "exact"):
+            raise ValueError(f"Invalid value of match parameter. Expect one of ('exact', 'prefix', 'contains'), got '{match}'")
+        name = f"match-{match}"
         if ignore_case:
             name = name + "-ignore-case"
         if ignore_punct:
@@ -33,6 +48,7 @@ class MatchMetric(NamedMetric):
         self.ignore_case = ignore_case
         self.ignore_punct = ignore_punct
         self.normalize_whitespace = normalize_whitespace
+        self.match = match
 
         super().__init__(name, self.__engine)
 
@@ -49,40 +65,11 @@ class MatchMetric(NamedMetric):
             strip_punct=self.ignore_punct,
             normalize_whitespace=self.normalize_whitespace,
         )
-        return self.match(expected, actual)
-
-    def match(self, expected: str, actual: str) -> float:
-        raise NotImplementedError()
-
-
-class SubstringMetric(MatchMetric):
-    """
-    Metric that checks that the actual response contains the expected string.
-    """
-
-    def match(self, expected: str, actual: str) -> float:
-        if expected in actual:
-            return 1.0
-        return 0.0
-
-
-class PrefixMetric(MatchMetric):
-    """
-    Metric that checks that the actual response starts with the expected string.
-    """
-
-    def match(self, expected: str, actual: str) -> float:
-        if actual.startswith(expected):
-            return 1.0
-        return 0.0
-
-
-class ExactMetric(MatchMetric):
-    """
-    Metric that checks that the actual response is exactly equal to the expected string.
-    """
-
-    def match(self, expected: str, actual: str) -> float:
-        if expected == actual:
-            return 1.0
-        return 0.0
+        if self.match == "contains":
+            return 1.0 if expected in actual else 0.0
+        elif self.match == "prefix":
+            return 1.0 if actual.startswith(expected) else 0.0
+        elif self.match == "exact":
+            return 1.0 if actual == expected else 0.0
+        else:
+            assert False  # not reached
