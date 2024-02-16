3e8775fe40928d14e6c214ecab9d35e7a1df3e25
Mike Kroutikov <mkroutikov@innodata.com>
2024-02-16 07:44:23 -0500

    draft of the "best of" metric

--- redlite/metric/__init__.py
+++ redlite/metric/__init__.py
@@ -1,5 +1,13 @@
 from ._basic import PrefixMetric, ExactMetric
 from ._random import RandomMetric
 from ._rejection import RejectionMetric
+from ._best_of import BestOfMetric, WorstOfMetric
 
-__all__ = ["RandomMetric", "PrefixMetric", "ExactMetric", "RejectionMetric"]
+__all__ = [
+    "RandomMetric",
+    "PrefixMetric",
+    "ExactMetric",
+    "RejectionMetric",
+    "BestOfMetric",
+    "WorstOfMetric",
+]
---
+++ redlite/metric/_best_of.py
@@ -0,0 +1,40 @@
+from .._core import NamedMetric
+
+
+class BestOf(NamedMetric):
+    """
+    Computes several metrics and chooses the best score.
+
+    Useful when we expect a rejection: we can use `MatchMetric` to detect rejection
+    and if rejection is not expected, we compute semantic closeness with BLEU or ROUGE.
+
+    - **metrics**
+    """
+
+    def __init__(self, *metrics: NamedMetric):
+        if len(metrics) < 1:
+            raise ValueError("Need at least one metric parameter")
+
+        metrics = sorted(metrics, key=lambda m: m.name)
+        self.metrics = metrics
+        name = "best-of-" + "-".join(m.name for m in metrics)
+        super().__init__(name, self.__engine)
+
+    def __engine(self, expected, actual) -> float:
+        scores = [m(expected, actual) for m in self.metrics]
+        return max(scores)
+
+
+class WorstOf(NamedMetric):
+    def __init__(self, *metrics: NamedMetric):
+        if len(metrics) < 1:
+            raise ValueError("Need at least one metric parameter")
+
+        metrics = sorted(metrics, key=lambda m: m.name)
+        self.metrics = metrics
+        name = "worst-of-" + "-".join(m.name for m in metrics)
+        super().__init__(name, self.__engine)
+
+    def __engine(self, expected, actual) -> float:
+        scores = [m(expected, actual) for m in self.metrics]
+        return min(scores)
