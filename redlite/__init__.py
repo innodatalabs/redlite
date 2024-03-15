d1b50293dd8cac0cb2a21707c0fd7ae49326d726
Mike Kroutikov <mkroutikov@innodata.com>
2024-03-15 12:01:34 -0400

    support for (almost) arbitrary HF model params

--- redlite/__init__.py
+++ redlite/__init__.py
@@ -10,7 +10,7 @@ from ._core import (
 from ._run import run, rescore
 from .dataset._load import load_dataset
 
-__version__ = "0.0.47"
+__version__ = "0.0.48"
 __all__ = [
     "run",
     "rescore",
