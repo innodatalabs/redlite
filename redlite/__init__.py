7f511e715c0c2f5c0850ccc813047729def771c2
Mike Kroutikov <mkroutikov@innodata.com>
2024-03-27 15:31:32 -0400

    latest legacy ui - heatmap chart

--- redlite/__init__.py
+++ redlite/__init__.py
@@ -10,7 +10,7 @@ from ._core import (
 from ._run import run, rescore
 from .dataset._load import load_dataset
 
-__version__ = "0.0.54"
+__version__ = "0.0.55"
 __all__ = [
     "run",
     "rescore",
--- redlite/server/resources/legacy/_app/immutable/entry/start.Wfdwzs58.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.x9Y7B9uX.js";export{t as start};
