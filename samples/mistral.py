735c4a4ec91fcbb34f0414785de8bd227a159601
Mike Kroutikov <mkroutikov@innodata.com>
2024-05-10 07:04:12 -0400

    fixed sample

--- samples/mistral.py
+++ samples/mistral.py
@@ -1,10 +1,13 @@
 import os
 from redlite import run, load_dataset
 from redlite.model.hf_model_pipeline import HFModelPipeline
+from redlite.model import IgnoreSystemModel
 from redlite.metric import MatchMetric
 
 
-model = HFModelPipeline("mistralai/Mistral-7B-Instruct-v0.2", token=os.environ["HF_TOKEN"])
+model = IgnoreSystemModel(
+    HFModelPipeline("mistralai/Mistral-7B-Instruct-v0.2", token=os.environ["HF_TOKEN"], device_map='auto')
+)
 dataset = load_dataset("hf:innodatalabs/rt-cogensumm")
 metric = MatchMetric(ignore_case=True, ignore_punct=True, strategy='prefix')
 
