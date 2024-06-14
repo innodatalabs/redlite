a674a87283d14a1ae30ea0d4d25cf28e782e9c31
Mike Kroutikov <mkroutikov@innodata.com>
2024-06-14 09:35:32 -0400

    support for nvidia api (openai-compatible)

--- redlite/model/openai_model.py
+++ redlite/model/openai_model.py
@@ -1,7 +1,8 @@
 from .. import NamedModel, MissingDependencyError
+from .._util import object_digest
 
 try:
-    from openai import OpenAI
+    from openai import OpenAI, NOT_GIVEN
 except ImportError as err:
     raise MissingDependencyError("Please install openai library") from err
 
@@ -15,16 +16,46 @@ class OpenAIModel(NamedModel):
     - **max_retries** (`int`): How many times to retry a failed request. Default is `2`.
     """
 
-    def __init__(self, model="gpt-3.5-turbo", api_key=None, max_retries=2):
+    def __init__(self,
+            *,
+            model="gpt-3.5-turbo",
+            base_url=None,
+            max_tokens=NOT_GIVEN,
+            temperature=NOT_GIVEN,
+            top_p=NOT_GIVEN,
+            api_key=None,
+            max_retries=2,
+    ):
+        self.base_url = base_url
         self.model = model
-        self.client = OpenAI(api_key=api_key, max_retries=max_retries)
+        self.max_tokens = max_tokens
+        self.temperature = temperature
+        self.top_p = top_p
+        self.client = OpenAI(api_key=api_key, max_retries=max_retries, base_url=base_url)
 
-        super().__init__(f"openai-{model}", self.__chat)
+        signature = {}
+        if base_url is not None:
+            signature['base_url'] = base_url
+        if max_tokens is not NOT_GIVEN:
+            signature['max_tokens'] = max_tokens
+        if temperature is not NOT_GIVEN:
+            signature['temperature'] = temperature
+        if top_p is not NOT_GIVEN:
+            signature['top_p'] = top_p
+
+        name = 'openai'
+        if len(signature) > 0:
+            name = f'openai-{object_digest(signature)[:6]}'
+
+        super().__init__(f"{name}-{model}", self.__chat)
 
     def __chat(self, messages: list) -> str:
         chat_completion = self.client.chat.completions.create(
             model=self.model,
             messages=messages,
+            max_tokens=self.max_tokens,
+            temperature=self.temperature,
+            top_p = self.top_p,
         )
 
         return chat_completion.choices[0].message.content
---
+++ samples/nvidia_llama_nim.py
@@ -0,0 +1,18 @@
+import os
+from redlite import run, load_dataset
+from redlite.model.openai_model import OpenAIModel
+from redlite.metric import MatchMetric
+
+
+model = OpenAIModel(
+    base_url="https://integrate.api.nvidia.com/v1",
+    api_key=os.environ["NVIDIA_API_KEY"],
+    model="meta/llama3-8b-instruct",
+    temperature=0.5,
+    top_p=1,
+    max_tokens=1024,
+)
+dataset = load_dataset("hf:innodatalabs/rt-gsm8k-gaia")
+metric = MatchMetric(ignore_case=True, ignore_punct=True, strategy='prefix')
+
+run(model=model, dataset=dataset, metric=metric)
