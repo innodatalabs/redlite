fb604fe75d05888ccdd68c046358f2456cf64247
Mike Kroutikov <mkroutikov@innodata.com>
2024-10-23 14:04:20 -0400

    fixing linting

--- redlite/model/llamacpp_model.py
+++ redlite/model/llamacpp_model.py
@@ -1,8 +1,5 @@
-import json
-
-from .._core import NamedModel, Message, MissingDependencyError, log
+from .._core import NamedModel, Message, MissingDependencyError
 from .._util import sha_digest
-from typing import Any
 
 try:
     import llama_cpp
@@ -25,10 +22,10 @@ class LlamaCppModel(NamedModel):
     ):
         self.llama = llama_cpp.Llama(model_path=model_path, **params)
 
-        name = "llama-cpp.." + model_path[-25:] + '@' + sha_digest({'model_path': model_path, **params })[:6]
+        name = "llama-cpp.." + model_path[-25:] + "@" + sha_digest({"model_path": model_path, **params})[:6]
 
         super().__init__(name, self.__predict)
 
     def __predict(self, messages: list[Message]) -> str:
-        response = self.llama.create_chat_completion(messages)
-        return response['choices'][0]['message']['content']
+        response = self.llama.create_chat_completion(messages)  # type: ignore[arg-type]
+        return response["choices"][0]["message"]["content"]  # type: ignore[index,return-value]
