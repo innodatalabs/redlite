90d76c0342e421e2e169d45c63940d71633ff3e5
Mike Kroutikov <mkroutikov@innodata.com>
2025-08-27 09:22:31 -0400

    default to non-streaming anthropic, support gemini system messages

--- samples/gemini.py
+++ samples/gemini.py
@@ -2,10 +2,9 @@ import os
 from redlite import run, load_dataset
 from redlite.model.gemini_model import GeminiModel
 from redlite.metric import MatchMetric
-from redlite.model import IgnoreSystemModel
 
 
-model = IgnoreSystemModel(GeminiModel(api_key=os.environ["GOOGLE_GEMINI_API_KEY"]))
+model = GeminiModel(api_key=os.environ["GOOGLE_GEMINI_API_KEY"])
 dataset = load_dataset("hf:innodatalabs/rt-cogensumm")
 metric = MatchMetric(ignore_case=True, ignore_punct=True, strategy='prefix')
 
