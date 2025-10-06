2f3165f4cee58e22119fca997ea4b8e47c651d5a
Mike Kroutikov <mkroutikov@innodata.com>
2025-10-06 12:07:26 -0400

    MakeSystemModel added (#45)

--- redlite/model/__init__.py
+++ redlite/model/__init__.py
@@ -1,5 +1,12 @@
-from ._ignore_system import IgnoreSystemModel, ConvertSystemToUserModel
+from ._ignore_system import IgnoreSystemModel, MakeSystemModel, ConvertSystemToUserModel
 from ._dummy import ParrotModel, CannedModel
 from ._throttle import ThrottleModel
 
-__all__ = ["IgnoreSystemModel", "ConvertSystemToUserModel", "ParrotModel", "CannedModel", "ThrottleModel"]
+__all__ = [
+    "IgnoreSystemModel",
+    "MakeSystemModel",
+    "ConvertSystemToUserModel",
+    "ParrotModel",
+    "CannedModel",
+    "ThrottleModel",
+]
