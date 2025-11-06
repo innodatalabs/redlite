304484cea6ab04bf641266e10341bcaaf082a102
Mike Kroutikov <mkroutikov@innodata.com>
2025-11-06 18:41:50 -0500

    fixed livecodebench sample

--- samples/benchmark_livecodebench.py
+++ samples/benchmark_livecodebench.py
@@ -1,14 +1,13 @@
-from redlite import run, rescore
+from redlite import run
 from redlite.benchmark.livecodebench import get_dataset, get_metric
 from redlite.model.openai_model import OpenAIModel
 import os
 
 
-# model = OpenAIModel(model="o4-mini", api_key=os.environ["OPENAI_API_KEY"])
+model = OpenAIModel(model="o4-mini", api_key=os.environ["OPENAI_API_KEY"])
 
-# dataset = get_dataset()
+dataset = get_dataset()
 
 metric = get_metric()
 
-# run(model=model, dataset=dataset, metric=metric)
-rescore(run='cool-staff-290', metric=metric)
+run(model=model, dataset=dataset, metric=metric)
